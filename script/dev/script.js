bed26ef396fbd2cd4bf47e00885ac5a2893ea4de
kanstantsin.zayats <kanstantsin.zayats@altoros.com>
2022-03-25 02:58:22 +0300

    Main slider

---
+++ script/dev/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+  $('.main-slider').slick({
+    lazyLoad: 'ondemand',
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    slidesToShow: 1
+  });
+});
